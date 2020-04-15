675cba76e516ce2d5a32296bf1e3f61558bce296
lynklody <liuai_1995@live.cn>
2020-04-15 22:58:31 +0800

    +: 1st fb config

---
+++ src/config/fbConfig.js
@@ -0,0 +1,23 @@
+ import firebase from 'firebase/app'   // core functionality of fb
+ import 'firebase/firestore'           // database
+ import 'firebase/auth'
+ 
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAIaA5qkCnTKM3IAZL52Mee-XzDN3Llf84",
+    authDomain: "todo-backend-4142d.firebaseapp.com",
+    databaseURL: "https://todo-backend-4142d.firebaseio.com",
+    projectId: "todo-backend-4142d",
+    storageBucket: "todo-backend-4142d.appspot.com",
+    messagingSenderId: "314294832672",
+    appId: "1:314294832672:web:21134312475225f7833645",
+    measurementId: "G-K7ZC69V9TC"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  firebase.firestore().settings({ timestampsInSnapShots: true})
+
+  export default firebase;
