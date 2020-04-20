8d115a133808c1ac8fa4b0fd8fa13ad463bf542c
lynklody <liuai_1995@live.cn>
2020-04-20 19:27:55 +0800

    +: connect firebase repo to store

--- src/index.js
+++ src/index.js
@@ -10,16 +10,27 @@ import loggerEnhancer from "./enhancers/logger"
 import { MuiThemeProvider } from '@material-ui/core'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from './theme'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import fbConfig from './config/fbConfig'
 // import * as serviceWorker from './serviceWorker';
 // import store from './store'
 // import './index.css';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, 
-    composeEnhancers(
-        applyMiddleware(thunkMiddleware, loggerMiddleware),
-        loggerEnhancer
-        ));
+    compose(
+    // composeEnhancers(
+        // loggerEnhancer,
+        applyMiddleware(thunkMiddleware.withExtraArgument({  // define passing extra arguments to actions frmo here
+                    getFirebase, 
+                    getFirestore 
+                }), 
+            loggerMiddleware),
+        reduxFirestore(fbConfig),
+        reactReduxFirebase(fbConfig)
+        )
+    );
 
 ReactDOM.render(
     <Provider store={store}>
