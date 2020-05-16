31ec7a93d2f14f752ced45d6ab5f9919aa094436
lynklody <liuai_1995@live.cn>
2020-05-17 03:32:18 +0800

    *: filter
    need to fix more than this to make it functioning

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,20 +1,57 @@
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { useSelector } from 'react-redux'
+
 // used in AddTodoContainer
 export const getText = (state) => state.text
+// export const getText = (state, id) => state.firestore.ordered.todos[id].todo.text
 
 // used in TodoListContainer / Footer Container
 export const getFilter = (state) => state.filter
+// export const getFilter = (state, id) => state.firestore.ordered.todos[id].todo.filter
 
 // used in TodoListContainer
-export const getVisibleTodos = (state) => {
-    const { todos: {data}, filter } = state
-    switch (filter) {
-        case 'all':
-            return data
-        case 'completed':
-            return data.filter(t => t.completed)
-        case 'active':
-            return data.filter(t => !t.completed)
-        default:
-            return new Error('Unknown filter: ' + filter)
-    }
+export const getVisibleTodos = (todos) => {
+    // const { todos: {data}, filter } = state
+        return todos.map( (todo) => 
+            {
+                const { completed, filter } = todo
+                switch(filter) {
+                    case 'all':
+                        return todo
+                    case 'active':
+                        if (!completed) {
+                            return todo
+                        } 
+                        break;
+                    case 'completed':
+                        if (completed) {
+                            return todo
+                        } 
+                        break;
+                    default:
+                        return new Error('Unknown filter: ' + todo.filter)
+                }
+                return todos
+            }
+        )
+        // switch (todos.filter) {
+        //     case 'all':
+        //         return todos
+        //     case 'completed':
+        //         // return text.filter(t => t.completed)
+        //         if (todos.completed) {
+        //             return todos
+        //         }
+        //         break
+        //     case 'active': 
+        //         // return text.filter(t => !t.completed)
+        //         if (!todos.completed) {
+        //             return todos
+        //         }
+        //         break
+        //     default:
+        //         return new Error('Unknown filter: ' + todos.filter)
+        // }
 }
