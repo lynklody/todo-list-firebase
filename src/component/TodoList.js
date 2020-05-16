4a6b31eea9448320ed2a143e9619278c09e66dcd
lynklody <liuai_1995@live.cn>
2020-05-16 19:23:58 +0800

    +: 1st successful query from firestore db
    TODO: 
    fix broken links: 
        getVisibleTodo()
        toggleTodo()

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import Todo from "./Todo";
 import { CardContent, Container } from '@material-ui/core'
 import { fetchTodos, fetchTodosFirebase } from '../actions'
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
+import { useSelector } from 'react-redux'
 
 // class TodoList extends Component {
 
@@ -10,28 +12,57 @@ import { fetchTodos, fetchTodosFirebase } from '../actions'
         // this.props.fetchTodosFirebase();
     // }
 
-const TodoList = (props) => {
-    useEffect(() => {
-        // fetchTodos();
-        fetchTodosFirebase();
-    }, [])
+// const TodoList = (props) => {
+    const TodoList = ({firebase, todos}, props) => {
+    // useEffect(() => {
+    //     // fetchTodos();
+    //     fetchTodosFirebase();
+    // }, [])
+
     // render() {
         // const {todos, toggleTodo} = this.props;
+    // console.log("TODOLIST compo",todos)
+    if (!isLoaded(todos)) {
         return (
             <Container>
                 <CardContent>
-                    <ul>
-                        {
-                            props.todos.map(todo => {
-                            // return <li> {todo.text} </li>
-                            return <Todo key={todo.id} {...todo} 
-                                onClick={() => {props.toggleTodo(todo.id)}} />
-                            })
-                        }
-                    </ul>
+                    Loading...
                 </CardContent>
             </Container>
-        );
+        )
+    }
+    if (isEmpty(todos)) {
+        return (
+            <Container>
+                <CardContent>
+                    :( Todo list is empty
+                </CardContent>
+            </Container>
+        )
+    }
+    return (   // isLoaded() && !isEmpty()
+        <Container>
+            <CardContent>
+                {/* <ul>
+                    {
+                        props.todos.map(todo => {
+                        // return <li> {todo.text} </li>
+                        return <Todo key={todo.id} {...todo} 
+                            onClick={() => {props.toggleTodo(todo.id)}} />
+                        })
+                    }
+                </ul> */}
+                <ul>
+                    { 
+                        todos.map(todo => {
+                            return <Todo key={todo.id} {...todo} 
+                             />                           
+                        })
+                    }
+                </ul>
+            </CardContent>
+        </Container>
+    );
     // }
 }
 
