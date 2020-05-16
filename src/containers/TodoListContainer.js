612ffccdc22dc3e69d9e187332a93a568d304b82
lynklody <liuai_1995@live.cn>
2020-05-16 19:26:30 +0800

    +: ...and this

--- src/containers/TodoListContainer.js
+++ src/containers/TodoListContainer.js
@@ -18,39 +18,44 @@ import { compose } from 'redux'
 //     }
 // }
 
-const mapStateToProps = (state) => {
-    // todos: getVisibleTodos(state.todos.data, state.filter)
-    // (state, props) => ({
-    //     todos: state.firestore.ordered.todos && state.firestore.ordered.todos[props.id]
-    //   })
-    // const todoslice = state.firestore.ordered.todos && state.firestore.ordered.todos[props.id]
-    return {
-        todos: getVisibleTodos(state.firestore.ordered.todos)
-    }
+// const mapStateToProps = (state) => {
+//     // todos: getVisibleTodos(state.todos.data, state.filter)
+//     return {
+//         // todos: getVisibleTodos(state.firestore.ordered.todos)
+//         todos: getVisibleTodos(state)
+//     }
+// }
+
+const mapStateToProps = ({ firestore: { ordered } }) => {
+    return ({
+        todos: ordered.todos,
+    })
 }
+
+
 
 const mapDispatchToProps = (dispatch) => ({
     toggleTodo: id => dispatch(toggleTodo(id)),
     // fetchTodos: () => dispatch(fetchTodos()),
-    fetchTodosFirebase: (state) => dispatch(fetchTodosFirebase(state))
+    // fetchTodosFirebase: (state) => dispatch(fetchTodosFirebase(state))
 })
 
-export default compose(
-    firestoreConnect(() => [ // connected to rootreducer firestoreReducer property
-        { collection: 'todos' }
-    ]),
-    connect(
-        mapStateToProps,
-        mapDispatchToProps
-    )
-)(TodoList);
-
 // export default compose(
-//     firestoreConnect((props) => [ // connected to rootreducer firestoreReducer property
-//         { collection: 'todos', doc: props.id }
+//     firestoreConnect(() => [ // connected to rootreducer firestoreReducer property
+//         { collection: 'todos' }
 //     ]),
 //     connect(
 //         mapStateToProps,
 //         mapDispatchToProps
 //     )
 // )(TodoList);
+
+export default compose(
+    firestoreConnect((props) => [ // connected to rootreducer firestoreReducer property
+        { collection: 'todos' }   // under state.firestore.data
+    ]),
+    connect(
+        mapStateToProps,
+        // mapDispatchToProps
+    )
+)(TodoList);
