7c0b5ebae48429a238b98bbc09743d12059310f7
lynklody <liuai_1995@live.cn>
2021-01-03 14:01:38 -0800

    + ocr with eng only

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import { Container, Box, Button, Grid, Typography } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
+import { Container, Box, Button, Grid, Typography, CardContent } from '@material-ui/core'
 import TextField from '@material-ui/core/TextField'
 import AddBoxIcon from '@material-ui/icons/AddBox'
-
+import CameraAltIcon from '@material-ui/icons/CameraAlt'
+import { createWorker } from 'tesseract.js'
+import { SettingsOverscanRounded } from '@material-ui/icons'
 
 const AddTodo = (props) => {
 // class AddTodo extends Component {
@@ -33,12 +35,36 @@ const AddTodo = (props) => {
     
     const handleClick = () => {
         if (props.text !== "") {
+            console.log("text",props.text)
             props.addTodo(props.text)
             props.setTodoText("") // clear the content after input
         } else {
             console.log("Tell user the textfield is empty!");
         }
     }
+
+    // OCR (English Only)
+    const worker = createWorker({
+        logger: m => console.log(m)
+    })
+    const [ocr, setOcr] = useState('...')
+    // FOR TEST
+    // const imgLink = 'https://tesseract.projectnaptha.com/img/eng_bw.png'
+    const imgLink = './eng.png'
+    // const imgLink = './zh.png'
+    const handleOCR = async () => {
+        await worker.load()
+        await worker.loadLanguage('eng')
+        await worker.initialize('eng')
+        const {data: { text }} = await worker.recognize(imgLink)
+        setOcr(text)
+    }
+
+    useEffect(() => {
+        // handleOCR();
+        props.setTodoText(ocr)
+    })
+
 
     // render() {
         return (
@@ -56,7 +82,7 @@ const AddTodo = (props) => {
                     direction="row"
                     justify="center"
                     alignItems="center"
-                    spacing={3}
+                    spacing={1}
                     >
                     <Grid item xs={8}>
                         {/* <FormRow /> */}
@@ -73,7 +99,7 @@ const AddTodo = (props) => {
                         rows={2}
                         margin="normal"
                         value={props.text}
-                        onChange={handleChange}
+                        onChange={(e)=>handleChange(e)}
                     />
                     </Grid>
                     {/* <input value={this.props.text} onChange={this.handleChange} /> */}
@@ -81,6 +107,7 @@ const AddTodo = (props) => {
                         onClick={this.handleClick}
                         >Add</button> */}
                     <Grid item xs={2}>
+                        {console.log(props.text)}
                     <Button 
                         variant="outlined"
                         onClick={handleClick}
@@ -90,6 +117,15 @@ const AddTodo = (props) => {
                             Add
                     </Button>
                     </Grid>
+                    <Button 
+                        variant="outlined"
+                        onClick={handleOCR}
+                        startIcon={<CameraAltIcon />}
+                        color="primary"
+                        >
+                            Read from Img
+                    </Button>
+                    <CardContent>{ocr}</CardContent>
                 </Grid>
             </Typography>
             </Box>
